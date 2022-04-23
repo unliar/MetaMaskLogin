50a28ccdb75053fc7340023c32e97c64e92403af
blessyou <370732889@qq.com>
2022-04-23 20:34:25 +0800

    feat: update readme

--- functions/api/nonce/[token].ts
+++ functions/api/nonce/[token].ts
@@ -3,16 +3,31 @@ export async function onRequestGet(ctx) {
     params: { token },
     env,
   } = ctx;
-  const md5 = crypto.subtle.digest("SHA-256", new TextEncoder().encode(token));
-  await env.METAMASK_KV.put(`nonce:${token}`, md5);
+  try {
+    const md5 = crypto.subtle.digest(
+      "SHA-256",
+      new TextEncoder().encode(token)
+    );
+    await env.METAMASK_KV.put(`nonce:${token}`, md5);
 
-  return new Response(
-    JSON.stringify({
-      token,
-      nonce: md5,
-    }),
-    {
-      headers: { "Content-Type": "application/json" },
-    }
-  );
+    return new Response(
+      JSON.stringify({
+        data: {
+          token,
+          nonce: md5,
+        },
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        code: 500,
+        message: error.message,
+      }),
+      { headers: { "Content-Type": "application/json" } }
+    );
+  }
 }
